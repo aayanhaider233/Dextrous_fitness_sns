fc84d0819a807e5b5b433002647f438a8293212f
Aayan Haider <aayanhaider112358@gmail.com>
2025-05-11 11:17:57 +0600

    Update explore.php

--- explore.php
+++ explore.php
@@ -59,9 +59,8 @@ try {
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Dextrous - Profile</title>
+<title>Dextrous - Explore</title>
 <link href="images/favicon.png" rel="shortcut icon" type="image/x-icon" />
-<link rel="stylesheet" href="assets/css/style.css"> 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <style>
     :root {
